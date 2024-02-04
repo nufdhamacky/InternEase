a60e826f74d36a4bbc23746e5b2cdcb17205a446
Nufdha <mmfnufdha@gmail.com>
2024-02-04 21:41:15 +0530

    Update addAd.view.php

--- app/view/company/addAd.view.php
+++ app/view/company/addAd.view.php
@@ -29,113 +29,85 @@
                 
             </div>
 
-        <form method="POST">    
-            <div class="details">
-                <div class="heading-advertisement">
-                        <h2>Add Advertisements</h2>
-                </div>
-                <div class="compdetails">
+            <form action="<?=ROOT?>/company/addNewAd" method="POST">    
+                <div class="details">
+                    <div class="heading-advertisement">
+                            <h2>Add Advertisements</h2>
+                    </div>
+                    <div class="compdetails">
 
-                <?php
-                  //  if(isset($_SESSION['status'])){
-                    //    echo "<h6>".$_SESSION['status']. "</h6>";
-                   //    unset($_SESSION['status']);
-                    //}
-                ?>
+                    <?php
+                    //  if(isset($_SESSION['status'])){
+                        //    echo "<h6>".$_SESSION['status']. "</h6>";
+                    //    unset($_SESSION['status']);
+                        //}
+                    ?>
 
-                    <form action="<?=ROOT?>/app/controller/company.php" method="POST">
-                        <h4>Position</h4>
-                        <select name="position">
-                            <option value = "se">Sofware Engineer</option>
-                            <option value = "qa">Quality Assuarance</option>
-                            <option value = "ba">Business Analyst</option>
-                        </select>
-                        <br>
-
-                        <h4>Requirements</h4>
-                        <br>
-
-                        <div class="row">
-                            <label for="req1">React</label><br>
-                            <input type="checkbox" id="req1" name="req1">
-                        </div>
-                        <div class="row">
-                            <label for="req2">.Net</label><br>
-                            <input type="checkbox" id="req2" name="req2">
-                        </div>
-                        <div class="row">
-                            <label for="req3">Node</label><br>
-                            <input type="checkbox" id="req3" name="req3">
-                        </div>
-                        <div class="row">
-                            <label for="req4">Java</label><br>
-                            <input type="checkbox" id="req4" name="req4">
-                        </div>
-                        <div class="row">
-                            <label for="req1">JavaScript</label><br>
-                            <input type="checkbox" id="req1" name="req1">
-                        </div>
-                        <div class="row">
-                            <label for="req5">DevOps</label><br>
-                            <input type="checkbox" id="req5" name="req5">
-                        </div>
-                        <div class="row">
-                            <label for="req6">C++</label><br>
-                            <input type="checkbox" id="req6" name="req6">
-                        </div>
-                        <div class="row">
-                            <label for="req6">SQL</label><br>
-                            <input type="checkbox" id="req6" name="req6">
-                        </div>
-                        <div class="row">
-                            <label for="req6">Python</label><br>
-                            <input type="checkbox" id="req6" name="req6">
-                        </div>
-                        <br>
-
-                        <!-- <input name="requirement" type = "text" class = "bx1"> -->
-                    
-                        <h4>Qualifications</h4>
-                        <br>
-
-                        <select name="position">
-                            <option value = "cs">BSc. in Computer Science</option>
-                            <option value = "is">BSc. in Information systems</option>
-                            <option value = "cs-sp">BSc.(Hons) in Computer Science</option>
-                            <option value = "is-sp">BSc.(Hons) in Information systems</option>
-                        </select>
-                        <br>
-
-                        <br>
                         
-                        <h4>Internship Period</h4>
-                        <div class = "card">
-                            <input name="start_date" class="ad-date" type = "month" class = "bx1">
-                            <h4 class = "h4">to</h4>
-                            <input name="end_date" class="ad-date" type = "month" class = "bx1">
-                        </div>
-                        <br>
-
-                        <div class = "card">
-                            <h4>No. of Interns</h4>
-                            <h4 class = "spaceleft" style="margin-left:150px">Working Mode</h4>
-                        </div>
-                        <div class = "card">
-                            <input name="no_of_intern" type = "number" class = "bx1">
-                            <select name="working_mode">
-                                <option value = "Online">Online</option>
-                                <option value = "Physical">Physical</option>
-                                <option value = "Hybrid">Hybrid</option>
+                            <h4>Position</h4>
+                            <select name="position">
+                                <option value = "" selected hidden>Select Position</option>
+                                <option value = "se">Sofware Engineer</option>
+                                <option value = "qa">Quality Assuarance</option>
+                                <option value = "ba">Business Analyst</option>
                             </select>
-                        </div>
+                            <br>
+
+                            <h4>Requirements</h4>
+                            <br>
+
+                            <label for="req1"><input type="checkbox" name="req"> React</label><br>
+                            <label for="req2"><input type="checkbox" name="req"> .Net</label><br>
+                            <label for="req3"><input type="checkbox" name="req"> Node</label><br>
+                            <label for="req4"><input type="checkbox" name="req"> Java</label><br>
+                            <label for="req5"><input type="checkbox" name="req"> JavaScript</label><br>
+                            <label for="req6"><input type="checkbox" name="req"> DevOps</label><br>
+                            <label for="req7"><input type="checkbox" name="req"> C++</label><br>
+                            <label for="req8"><input type="checkbox" name="req"> SQL</label><br>
+                            <label for="req9"><input type="checkbox" name="req"> Python</label><br>
                         
-                        <div class="submit">
-                            <button type="submit" name="submit" value="saveAd">SAVE</button>
-                        </div>
-                    </form>
+                            <h4>Qualifications</h4>
+                            <br>
+
+                            <select name="position">
+                                <option value = "" selected hidden>Select Qualification</option>
+                                <option value = "cs">BSc. in Computer Science</option>
+                                <option value = "is">BSc. in Information systems</option>
+                                <option value = "cs-sp">BSc.(Hons) in Computer Science</option>
+                                <option value = "is-sp">BSc.(Hons) in Information systems</option>
+                            </select>
+                            <br>
+
+                            <br>
+                            
+                            <h4>Internship Period</h4>
+                            <div class = "card">
+                                <input name="start_date" class="ad-date" type = "month" class = "bx1">
+                                <h4 class = "h4">to</h4>
+                                <input name="end_date" class="ad-date" type = "month" class = "bx1">
+                            </div>
+                            <br>
+
+                            <div class = "card">
+                                <h4>No. of Interns</h4>
+                                <h4 class = "spaceleft" style="margin-left:150px">Working Mode</h4>
+                            </div>
+                            <div class = "card">
+                                <input name="no_of_intern" type = "number" class = "bx1">
+                                <select name="working_mode">
+                                    <option value = "Online">Online</option>
+                                    <option value = "Physical">Physical</option>
+                                    <option value = "Hybrid">Hybrid</option>
+                                </select>
+                            </div>
+                            
+                            <div class="submit">
+                                <button type="submit" name="submit" value="saveAd">SAVE</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
         
         </div>
     </div>
