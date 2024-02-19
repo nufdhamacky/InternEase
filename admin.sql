e0f669ce65f13a1c900b4cc3145525af09885d0e
iamgian2001 <ggogamer60@gmail.com>
2024-02-19 09:47:39 +0530

    admin db and complaint db

--- admin.sql
+++ admin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 13, 2024 at 05:10 AM
+-- Generation Time: Feb 19, 2024 at 04:14 AM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
@@ -29,25 +29,25 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
-  `Admin_ID` int NOT NULL AUTO_INCREMENT,
-  `UserName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `Email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `admin_id` int NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Email` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `Password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `FirstName` varchar(255) NOT NULL,
   `LastName` varchar(255) NOT NULL,
-  `Phone` int NOT NULL,
-  PRIMARY KEY (`Admin_ID`),
-  UNIQUE KEY `adminid` (`Admin_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `Phone` varchar(12) NOT NULL,
+  PRIMARY KEY (`admin_id`),
+  UNIQUE KEY `adminid` (`admin_id`),
+  KEY `UserName` (`UserName`,`Email`),
+  KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`Admin_ID`, `UserName`, `Email`, `Password`, `FirstName`, `LastName`, `Phone`) VALUES
-(6, 'Gesad', '2121@gmail.com', '12345', '', '', 0),
-(4, 'Geesara', 'sadds@gmail.com', '$2y$10$2hpIHjOlZoO4qrf9OSK8vO9zIiuTHncupyQ.7ga.jq.F83Q.w3Fhi', '', '', 0),
-(1, 'root', 'root@gmail.com', '$2y$10$XtJECUtXzxqWMgLktc67GOtysbLbVWwTC3GUZVlReCLNWzlOvGx3S', '', '', 0);
+INSERT INTO `admin` (`admin_id`, `UserName`, `Email`, `Password`, `FirstName`, `LastName`, `Phone`) VALUES
+(3, 'Geesara Gawesh', 'sdasd@gmail.com', '$2y$10$VCuBlOn5XHAkIcE8svL7Muv0ktjCguenLl6.kXhw5yHBcXlY2PWW.', 'Geesara', 'Gawesh', '77542854');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
