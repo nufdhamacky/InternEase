12e9eb14c369699ad47a52f44dc9a987250e590d
Shamah <shamahfathima2020@gmail.com>
2024-02-24 16:54:40 +0530

    Ad-cards sticky

--- app/view/student/dashboard.view.php
+++ app/view/student/dashboard.view.php
@@ -8,11 +8,6 @@
         <div class="main">
             <?php require_once("../app/view/inc/topbar.php"); ?>
             
-            <div class="wrapper">
-                <div class="application-status">
-                    hello
-                </div>
-            </div>
                 
         </div>
             
