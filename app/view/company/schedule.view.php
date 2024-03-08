d2f15ccaecf55a744d65a4d53ffa132bcb7044b2
Nufdha <mmfnufdha@gmail.com>
2024-03-08 15:43:12 +0530

    updated schedule file

--- app/view/company/schedule.view.php
+++ app/view/company/schedule.view.php
@@ -27,185 +27,25 @@
         <!--student data list-->
         <div class="details">
             <div class="studentdetails">
-                <div class="submit">
-                    <a href="tech">
-                        <button type="submit" class="sbtn">SCHEDULE TECH-TALK</button>
-                    </a>
-                    <!-- <a href="companyVisit">
-                        <button type="submit"class="sbtn">SCHEDULE COMPANY VISIT</button>
-                    </a> -->
-                </div>
-                
-                <div class = "cardHeader">
-                    <h2>Interview Schedules</h2>
-                </div>
-                <table>
-                    <thead>
-                        <tr>
-                            <td>Student Name</td>
-                            <td>Registration No.</td>
-                            <td>Position</td>
-                            <td>Date Period</td>
-                            <td>Interview Duration</td>
-                            <td>Day</td>
-                        </tr>
-                    </thead>
+                <input type="radio" name="slider" id="intschedule" checked>
+                <input type="radio" name="slider" id="techschedule">
 
-                    <tbody>
-                        <tr>
-                            <td>Hamsayini Senthilrasa</td>
-                            <td>2021/CS/065</td>
-                            <td>Software Engineer</td>
-                            <td>02/07/2024 - 09/07/2024</td>
-                            <td>40 mins</td>
-                            <td>Monday</td>
-                            <td>
-                                <a href="scheduleInt">
-                                    <ion-icon name="create-outline"></ion-icon>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="delete">
-                                    <ion-icon name="trash-outline" class="del"></ion-icon>
-                                </a>
-                            </td>
-                        </tr>
+                <nav>
+                    <label for="intschedule" class="intschedule">Student Interview Schedule</label>
+                    <label for="techschedule" class="techschedule">Tech-Talk Schedule</label>
+                    <div class="slider"></div>
+                </nav>
 
-                        <tr>
-                            <td>Nufdha Macky</td>
-                            <td>2021/CS/058</td>
-                            <td>Software Engineer</td>
-                            <td>02/07/2024 - 09/07/2024</td>
-                            <td>40 mins</td>
-                            <td>Monday</td>
-                            <td>
-                                <a href="scheduleInt">
-                                    <ion-icon name="create-outline"></ion-icon>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="delete">
-                                    <ion-icon name="trash-outline" class="del"></ion-icon>
-                                </a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Gien Gawesh</td>
-                            <td>2021/IS/024</td>
-                            <td>Software Engineer</td>
-                            <td>02/07/2024 - 09/07/2024</td>
-                            <td>40 mins</td>
-                            <td>Monday</td>
-                            <td>
-                                <a href="scheduleInt">
-                                    <ion-icon name="create-outline"></ion-icon>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="delete">
-                                    <ion-icon name="trash-outline" class="del"></ion-icon>
-                                </a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Shamah Lafir</td>
-                            <td>2021/IS/031</td>
-                            <td>Software Engineer</td>
-                            <td>02/07/2024 - 09/07/2024</td>
-                            <td>40 mins</td>
-                            <td>Monday</td>
-                            <td>
-                                <a href="scheduleInt">
-                                    <ion-icon name="create-outline"></ion-icon>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="delete">
-                                    <ion-icon name="trash-outline" class="del"></ion-icon>
-                                </a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Nufdha Macky</td>
-                            <td>2021/CS/014</td>
-                            <td>Software Engineer</td>
-                            <td>02/07/2024 - 09/07/2024</td>
-                            <td>40 mins</td>
-                            <td>Monday</td>
-                            <td>
-                                <a href="scheduleInt">
-                                    <ion-icon name="create-outline"></ion-icon>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="delete">
-                                    <ion-icon name="trash-outline" class="del"></ion-icon>
-                                </a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Hamsayini Senthilrasa</td>
-                            <td>2021/IS/027</td>
-                            <td>Software Engineer</td>
-                            <td>02/07/2024 - 09/07/2024</td>
-                            <td>40 mins</td>
-                            <td>Monday</td>
-                            <td>
-                                <a href="scheduleInt">
-                                    <ion-icon name="create-outline"></ion-icon>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="delete">
-                                    <ion-icon name="trash-outline" class="del"></ion-icon>
-                                </a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Gien Gawesh</td>
-                            <td>2021/IS/045</td>
-                            <td>Software Engineer</td>
-                            <td>02/07/2024 - 09/07/2024</td>
-                            <td>40 mins</td>
-                            <td>Monday</td>
-                            <td>
-                                <a href="scheduleInt">
-                                    <ion-icon name="create-outline"></ion-icon>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="delete">
-                                    <ion-icon name="trash-outline" class="del"></ion-icon>
-                                </a>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td>Shamah Lafir</td>
-                            <td>2021/CS/085</td>
-                            <td>Software Engineer</td>
-                            <td>02/07/2024 - 09/07/2024</td>
-                            <td>40 mins</td>
-                            <td>Monday</td>
-                            <td>
-                                <a href="scheduleInt">
-                                    <ion-icon name="create-outline"></ion-icon>
-                                </a>
-                            </td>
-                            <td>
-                                <a href="delete">
-                                    <ion-icon name="trash-outline" class="del"></ion-icon>
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-
+                <section>
+                    <div class="content content-1">
+                        <div class="title">Interview Schedules of Students</div>
+                        <p>Interview schedule table included</p>
+                    </div>
+                    <div class="content content-2">
+                        <div class="title">Tech-Talk Schedule</div>
+                        <p>Tech-Talk schedule table included</p>
+                    </div>
+                </section>
             </div>
         </div>
             
