dff2cf97eb3d328f1cbc408d90250b02e9be6115
Hamsayini <sayisenthil@gmail.com>
2024-03-09 15:29:02 +0530

    Design UI for company visit

--- app/view/pdc/schedule.view.php
+++ app/view/pdc/schedule.view.php
@@ -28,46 +28,59 @@ $pageData = $pdcController->getAllCompanyVisits($page);
                 <ion-icon class="profile-icon" name="person-circle-outline"></ion-icon>
             </div>
         </div>
-        <a href="addschedule" class="btn">Add</a>
-        <table>
-            <thead>
-            <tr>
-                <td>Company Name</td>
-                <td>Request Date</td>
-                <td>Visit Date</td>
-                <td>Reason</td>
-                <td>Status</td>
-                <td>Action</td>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            foreach ($pageData->data as $v) { ?>
-                <tr>
-                    <td><?php echo $v->companyName; ?></td>
-                    <td><?php echo $v->requestDate; ?></td>
-                    <td><?php echo $v->visitDate ?? "Not Set"; ?></td>
-                    <td><?php echo $v->reason; ?></td>
-                    <td style="color: <?php echo $v->status == 0 ? "blue" : ($v->status == 1 ? "green" : "red"); ?>"><?php echo $v->status == 0 ? "Pending" : ($v->status == 1 ? "Approved" : "Rejected"); ?></td>
-                    <td>
-                        <!--                        <a href="viewstudent?id=--><?php //echo $v->id; ?><!--">view</a>-->
-                    </td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
+        <div class="secondbar">
+            <div class="search">
+                <ion-icon name="search-outline"></ion-icon>
+                <input type="text" placeholder="Search Company" class="box1">
+            </div>
+        </div>
+        
+        <div id="seTable" class="details">
+            <div class="studentdetails">
+                <div class="cardHeader">
+                    <h2>Company Visit Schedule</h2>
+                    <a href="addschedule" class="btn">Add</a>
+                </div>
+                <table>
+                    <thead>
+                    <tr>
+                        <td>Company Name</td>
+                        <td>Request Date</td>
+                        <td>Visit Date</td>
+                        <td>Reason</td>
+                        <td>Status</td>
+                        <td>Action</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach ($pageData->data as $v) { ?>
+                        <tr>
+                            <td><?php echo $v->companyName; ?></td>
+                            <td><?php echo $v->requestDate; ?></td>
+                            <td><?php echo $v->visitDate ?? "Not Set"; ?></td>
+                            <td><?php echo $v->reason; ?></td>
+                            <td style="color: <?php echo $v->status == 0 ? "blue" : ($v->status == 1 ? "green" : "red"); ?>"><?php echo $v->status == 0 ? "Pending" : ($v->status == 1 ? "Approved" : "Rejected"); ?></td>
+                            <td>
+                                <!--                        <a href="viewstudent?id=-->
+                                <?php //echo $v->id; ?><!--">view</a>-->
+                            </td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
 
+            </div>
+        </div>
+        <div>
+            <a href="?course=<?php echo $_GET["course"] ?? "all"; ?>&page=<?php echo $pageData->currentPage - 1; ?>"
+               style="display: <?php echo $pageData->currentPage > 1 ? "inline" : "none"; ?>"
+               class="btn">Previous</a>
+            <a href="?course=<?php echo $_GET["course"] ?? "all"; ?>&page=<?php echo $pageData->currentPage + 1; ?>"
+               style="display: <?php echo $pageData->currentPage > 0 && $pageData->currentPage < $pageData->totalPages ? "inline" : "none"; ?>"
+               class="btn">Next</a>
+        </div>
     </div>
-</div>
-<div>
-    <a href="?course=<?php echo $_GET["course"] ?? "all"; ?>&page=<?php echo $pageData->currentPage - 1; ?>"
-       style="display: <?php echo $pageData->currentPage > 1 ? "inline" : "none"; ?>"
-       class="btn">Previous</a>
-    <a href="?course=<?php echo $_GET["course"] ?? "all"; ?>&page=<?php echo $pageData->currentPage + 1; ?>"
-       style="display: <?php echo $pageData->currentPage > 0 && $pageData->currentPage < $pageData->totalPages ? "inline" : "none"; ?>"
-       class="btn">Next</a>
-</div>
-</div>
 </div>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
