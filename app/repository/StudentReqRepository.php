9b57699490811dc34c1588b7af87e293ed6aab29
Nufdha <mmfnufdha@gmail.com>
2024-03-12 00:05:40 +0530

    Create StudentReqRepository.php

---
+++ app/repository/StudentReqRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once('../app/model/StudentReqModel.php');
+
+class StudentReqRepository{
+    private $conn;
+
+    public function __construct($conn){
+        $this->conn = $conn;
+    } 
+
+    public function getAllRequests(): array {
+        $sql = "SELECT * FROM student";
+        $result = $this->conn->query($sql);
+
+        $requests = [];
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $request = new StudentReqModel(
+                    $row['user_id'], // Assuming user_id is the correct column name
+                    $row['first_name'],
+                    $row['last_name'],
+                    $row['reg_no'],
+                );
+
+                $requests[] = $request;
+            }
+        }
+
+        return $requests;
+    }
+}
