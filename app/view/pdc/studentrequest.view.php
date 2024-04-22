6382a5dd41db69e0a84d972ea54f27430f3aa1ab
Hamsayini <sayisenthil@gmail.com>
2024-04-22 10:18:53 +0530

    Fix user issue

--- app/view/pdc/studentrequest.view.php
+++ app/view/pdc/studentrequest.view.php
@@ -4,141 +4,141 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Request</title>
-    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/css/pdc/ad.css">
+    <link rel="stylesheet" type="text/css" href="<?= ROOT ?>/css/pdc/ad.css">
 </head>
 <body>
 <div class="details">
-            <div class="internshipAds">
-                <div class = "cardHeader">
-                    <h2>Student Requests</h2>
-                </div>
-                <table>
-                    <thead>
-                        <tr>
-                            <td>Student Name</td>
-                            <td>Index No</td>
-                            <td>Email</td>
-                            <td>Round No</td>
-                            <td colspan="3">Applied Companies</td>
-                            <td colspan="3" >Job Roles</td>
-                            <td colspan="5" >View</td>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <tr>
-                            <td>Hamsayini</td>
-                            <td>21020337</td>
-                            <td>sayisenthil@gmail.com</td>
-                            <td>1</td>
-                            <td>IBM<br> Virtusa <br> 99X <br> CodeGen <br>Sysco Labs <br>    </td>
-                            <td colspan="5"></td>
-                            <td colspan="5"><a href="#" span class = "view"></span>View</td>
-                        </tr>
-                        
-                        <tr>
-                            <td>Nufdha</td>
-                            <td>21020698</td>
-                            <td>nufdha@gmail.com</td>
-                            <td>1</td>
-                            <td>WSO2 <br> Applexus <br> CodeGen <br> IFS <br>Sysco Labs <br> </td>
-                            <td colspan="5"></td>
-                            <td colspan="5"><a href="#" span class = "view"></span>View</td>
-                        </tr>
-
-                        <tr>
-                            <td>Achchu</td>
-                            <td>21020045</td>
-                            <td>achcu@gmail.com</td>
-                            <td>2</td>
-                            <td colspan="3"></td>
-                            <td colspan="6" > Business Analyst <br> Quality Assurance <br> Software Engineer <br></td>
-                            <td colspan="6"><a href="#" span class = "view"></span>View</td>
-                        </tr>
-
-                        <!-- <tr>
-                            <td>Sharmi</td>
-                            <td>21020445</td>
-                            <td>mini@gmail.com</td>
-                            <td>2</td>
-                            <td colspan="3"></td>
-                            <td> DataAnalyst <br> Developer <br> Software Engineer <br></td>
-                            <td colspan="5"><a href="#" span class = "view"></span>View</td>
-                        </tr> -->
-
-                        <!-- <tr>
-                            <td>IFS</td>
-                            <td>ifs@gmail.com</td>
-                            <td>
-                                <a href="https://www.ifs.com/" target="_blank">https://www.ifs.com/</a>
-                            </td>
-                            <td>Sayi </td>
-                            <td>0776453123</td>
-                            <td>
-                                <select class="status-select">
-                                    <option value="pending">Pending</option>
-                                    <option value="approved">Approved</option>
-                                    <option value="rejected">Rejected</option>
-                                </select>
-                            </td>
-                            <td><a href="#" span class = "view"></span>View</td>
-                        </tr>
-                        <tr>
-                            <td>Microsoft</td>
-                            <td>microsoft@gmail.com</td>
-                            <td>
-                                <a href="https://www.microsoft.com/en-lk" target="_blank">https://www.microsoft.com/en-lk</a>
-                            </td>
-                            <td>Sayi </td>
-                            <td>0776453123</td>
-                            <td>
-                                <select class="status-select">
-                                    <option value="pending">Pending</option>
-                                    <option value="approved">Approved</option>
-                                    <option value="rejected">Rejected</option>
-                                </select>
-                            </td>
-                            <td><a href="#" span class = "view"></span>View</td>
-                        </tr>
-                        <tr>
-                            <td>applexus</td>
-                            <td>applexus@gmail.com</td>
-                            <td>
-                                <a href="https://www.applexus.com/" target="_blank">https://www.applexus.com/</a>
-                            </td>
-                            <td>Sayi </td>
-                            <td>0776453123</td>
-                            <td>
-                                <select class="status-select">
-                                    <option value="pending">Pending</option>
-                                    <option value="approved">Approved</option>
-                                    <option value="rejected">Rejected</option>
-                                </select>
-                            </td>
-                            <td><a href="#" span class = "view"></span>View</td>
-                        </tr>
-                        <tr>
-                            <td>Sysco Labs</td>
-                            <td>sysco@gmail.com</td>
-                            <td>
-                                <a href="https://syscolabs.lk/" target="_blank">https://syscolabs.lk/</a>
-                            </td>
-                            <td>Sayi </td>
-                            <td>0776453123</td>
-                            <td>
-                                <select class="status-select">
-                                    <option value="pending">Pending</option>
-                                    <option value="approved">Approved</option>
-                                    <option value="rejected">Rejected</option>
-                                </select>
-                            </td>
-                            <td><a href="#" span class = "view"></span>View</td>
-                        </tr> -->
-                    </tbody>
-                </table>
-
-            </div>
+    <div class="internshipAds">
+        <div class="cardHeader">
+            <h2>Student Requests</h2>
         </div>
-        
+        <table>
+            <thead>
+            <tr>
+                <td>Student Name</td>
+                <td>Index No</td>
+                <td>Email</td>
+                <td>Round No</td>
+                <td colspan="3">Applied Companies</td>
+                <td colspan="3">Job Roles</td>
+                <td colspan="5">View</td>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td>Hamsayini</td>
+                <td>21020337</td>
+                <td>sayisenthil@gmail.com</td>
+                <td>1</td>
+                <td>IBM<br> Virtusa <br> 99X <br> CodeGen <br>Sysco Labs <br></td>
+                <td colspan="5"></td>
+                <td colspan="5"><a href="#" span class="view"></span></a>View</td>
+            </tr>
+
+            <tr>
+                <td>Nufdha</td>
+                <td>21020698</td>
+                <td>nufdha@gmail.com</td>
+                <td>1</td>
+                <td>WSO2 <br> Applexus <br> CodeGen <br> IFS <br>Sysco Labs <br></td>
+                <td colspan="5"></td>
+                <td colspan="5"><a href="#" span class="view"></span></a> View</td>
+            </tr>
+
+            <tr>
+                <td>Achchu</td>
+                <td>21020045</td>
+                <td>achcu@gmail.com</td>
+                <td>2</td>
+                <td colspan="3"></td>
+                <td colspan="6"> Business Analyst <br> Quality Assurance <br> Software Engineer <br></td>
+                <td colspan="6"><a href="#" span class="view"></span>View</a></td>
+            </tr>
+
+            <!-- <tr>
+                <td>Sharmi</td>
+                <td>21020445</td>
+                <td>mini@gmail.com</td>
+                <td>2</td>
+                <td colspan="3"></td>
+                <td> DataAnalyst <br> Developer <br> Software Engineer <br></td>
+                <td colspan="5"><a href="#" span class = "view"></span>View</td>
+            </tr> -->
+
+            <!-- <tr>
+                <td>IFS</td>
+                <td>ifs@gmail.com</td>
+                <td>
+                    <a href="https://www.ifs.com/" target="_blank">https://www.ifs.com/</a>
+                </td>
+                <td>Sayi </td>
+                <td>0776453123</td>
+                <td>
+                    <select class="status-select">
+                        <option value="pending">Pending</option>
+                        <option value="approved">Approved</option>
+                        <option value="rejected">Rejected</option>
+                    </select>
+                </td>
+                <td><a href="#" span class = "view"></span>View</td>
+            </tr>
+            <tr>
+                <td>Microsoft</td>
+                <td>microsoft@gmail.com</td>
+                <td>
+                    <a href="https://www.microsoft.com/en-lk" target="_blank">https://www.microsoft.com/en-lk</a>
+                </td>
+                <td>Sayi </td>
+                <td>0776453123</td>
+                <td>
+                    <select class="status-select">
+                        <option value="pending">Pending</option>
+                        <option value="approved">Approved</option>
+                        <option value="rejected">Rejected</option>
+                    </select>
+                </td>
+                <td><a href="#" span class = "view"></span>View</td>
+            </tr>
+            <tr>
+                <td>applexus</td>
+                <td>applexus@gmail.com</td>
+                <td>
+                    <a href="https://www.applexus.com/" target="_blank">https://www.applexus.com/</a>
+                </td>
+                <td>Sayi </td>
+                <td>0776453123</td>
+                <td>
+                    <select class="status-select">
+                        <option value="pending">Pending</option>
+                        <option value="approved">Approved</option>
+                        <option value="rejected">Rejected</option>
+                    </select>
+                </td>
+                <td><a href="#" span class = "view"></span>View</td>
+            </tr>
+            <tr>
+                <td>Sysco Labs</td>
+                <td>sysco@gmail.com</td>
+                <td>
+                    <a href="https://syscolabs.lk/" target="_blank">https://syscolabs.lk/</a>
+                </td>
+                <td>Sayi </td>
+                <td>0776453123</td>
+                <td>
+                    <select class="status-select">
+                        <option value="pending">Pending</option>
+                        <option value="approved">Approved</option>
+                        <option value="rejected">Rejected</option>
+                    </select>
+                </td>
+                <td><a href="#" span class = "view"></span>View</td>
+            </tr> -->
+            </tbody>
+        </table>
+
+    </div>
+</div>
+
 </body>
 </html>
