ef03ff7937dc83b331611aaeb88175d6a743049d
Shamah <shamahfathima2020@gmail.com>
2024-04-23 15:39:05 +0530

    temporary

--- app/core/config.php
+++ app/core/config.php
@@ -4,6 +4,8 @@ if($_SERVER['SERVER_NAME'] == 'localhost')
 {
 	/** database config **/
 	define('DBNAME', 'intern_ease');
+	// define('DBNAME', 'internEaseTest');
+
 	define('DBHOST', 'localhost');
 	define('DBUSER', 'root');
 	define('DBPASS', '');
