08b8c43baf831ed09090bf14a48f022859ee3e63
Hamsayini <sayisenthil@gmail.com>
2024-04-24 17:46:29 +0530

    working on complaint

--- app/repository/PdcComplaintRepository.php
+++ app/repository/PdcComplaintRepository.php
@@ -14,12 +14,12 @@ class PdcComplaintRepository
 
     public function getAll(): array
     {
-        $sql = "SELECT c.*,s.id, s.first_name,s.last_name , s.reg_no FROM complaint c JOIN student s ON c.student_id=s.id WHERE c.type = 'user_complaint'";
+        $sql = "SELECT c.*,s.id as student_id,s.user_id, s.first_name,s.last_name , s.reg_no FROM complaint c JOIN student s ON c.student_id=s.id WHERE c.type = 'user_complaint'";
         $result = $this->conn->query($sql);
         $list = [];
         while ($row = $result->fetch_assoc()) {
-            $student = new StudentShortModel($row["id"], $row["first_name"], $row["last_name"], $row["reg_no"]);
-            $complaint = new PdcComplaintModel($row["id"], $student, $row["type"], $row["title"], $row["description"], $row["reply"], $row["status"], $row["date"]);
+            $student = new StudentShortModel($row["student_id"], $row["user_id"], $row["first_name"], $row["last_name"], $row["reg_no"]);
+            $complaint = new PdcComplaintModel($row["complaint_id"], $student, $row["type"], $row["title"], $row["description"], $row["reply"], $row["status"], $row["created_at"]);
             $list[] = $complaint;
         }
         return $list;
