3b5464bc110570f990d7351e7cfdc15a5e9412a7
Hamsayini <sayisenthil@gmail.com>
2024-04-27 07:28:35 +0530

    get full details from ad

--- app/repository/StudentRepository.php
+++ app/repository/StudentRepository.php
@@ -21,7 +21,7 @@ class StudentRepository
         $userResult = $this->conn->query($userSql);
         if ($userResult) {
             $userId = $this->conn->insert_id;
-            
+
             $sql = "INSERT INTO student(user_id,email,first_name,last_name,index_no,reg_no) VALUES({$userId},'{$student->email}','{$student->firstName}', '{$student->lastName}',{$student->indexNo},'{$student->regNo}')";
             $result = $this->conn->query($sql);
 
@@ -67,7 +67,7 @@ class StudentRepository
             while ($r = $companyResult->fetch_assoc()) {
                 $company = new MyCompanyModel($r["user_id"], $r["company_name"], $r["email"], $r["contact_no"], $r["contact_person"], $r['company_site'],
                     1);
-                $ad = new CompanyAdModel($r["ad_id"], $r["position"], $r["requirements"], $r["no_of_intern"], $r["working_mode"], $r["from_date"], $r["to_date"], $r["company_id"], $r["qualification"], $company, $r["status"]);
+                $ad = new CompanyAdModel($r["ad_id"], $r["position"], $r["requirements"], $r["no_of_intern"], $r["working_mode"], $r["from_date"], $r["to_date"], $r["company_id"], $r["qualification"], $company, $r["status"], $r["no_of_cvs_required"]);
                 $ad->firstRoundData = new FirstRoundDataModel($r["apply_status"]);
                 $adList[] = $ad;
             }
