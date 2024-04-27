f4960c0cf082bbb0ee7ffd689849bdd3efda339c
iamgian2001 <ggogamer60@gmail.com>
2024-04-28 01:31:38 +0530

    tech talk progress

--- app/view/company/schedule.view.php
+++ app/view/company/schedule.view.php
@@ -4,17 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Schedule</title>
-
-    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/css/company/companySchedule.css">
-    
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script> 
+    <link rel="stylesheet" type="text/css" href="<?=ROOT?>/css/company/companySchedule.css">
 
+<style>
+.modal-backdrop{
+    z-index: -1;
+}
+</style>
 </head>
 <body>
     
@@ -47,7 +46,7 @@
                     <div class="slider"></div>
                 </nav>
 
-                <section>
+                <section class="section">
                     <div class="content content-1">
                         <!-- <div class="title">Interview Schedules of Students</div>
                         <p>Interview schedule table included</p> -->
@@ -87,148 +86,54 @@
                                     </td>
                                 </tr>
 
-                                <tr>
-                                    <td>Nufdha Macky</td>
-                                    <td>2021/CS/058</td>
-                                    <td>Software Engineer</td>
-                                    <td>02/07/2024 - 09/07/2024</td>
-                                    <td>40 mins</td>
-                                    <td>Monday</td>
-                                    <td>
-                                        <a href="scheduleInt">
-                                            <ion-icon name="create-outline"></ion-icon>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="delete">
-                                            <ion-icon name="trash-outline" class="del"></ion-icon>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>Gien Gawesh</td>
-                                    <td>2021/IS/024</td>
-                                    <td>Software Engineer</td>
-                                    <td>02/07/2024 - 09/07/2024</td>
-                                    <td>40 mins</td>
-                                    <td>Monday</td>
-                                    <td>
-                                        <a href="scheduleInt">
-                                            <ion-icon name="create-outline"></ion-icon>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="delete">
-                                            <ion-icon name="trash-outline" class="del"></ion-icon>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>Shamah Lafir</td>
-                                    <td>2021/IS/031</td>
-                                    <td>Software Engineer</td>
-                                    <td>02/07/2024 - 09/07/2024</td>
-                                    <td>40 mins</td>
-                                    <td>Monday</td>
-                                    <td>
-                                        <a href="scheduleInt">
-                                            <ion-icon name="create-outline"></ion-icon>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="delete">
-                                            <ion-icon name="trash-outline" class="del"></ion-icon>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>Nufdha Macky</td>
-                                    <td>2021/CS/014</td>
-                                    <td>Software Engineer</td>
-                                    <td>02/07/2024 - 09/07/2024</td>
-                                    <td>40 mins</td>
-                                    <td>Monday</td>
-                                    <td>
-                                        <a href="scheduleInt">
-                                            <ion-icon name="create-outline"></ion-icon>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="delete">
-                                            <ion-icon name="trash-outline" class="del"></ion-icon>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>Hamsayini Senthilrasa</td>
-                                    <td>2021/IS/027</td>
-                                    <td>Software Engineer</td>
-                                    <td>02/07/2024 - 09/07/2024</td>
-                                    <td>40 mins</td>
-                                    <td>Monday</td>
-                                    <td>
-                                        <a href="scheduleInt">
-                                            <ion-icon name="create-outline"></ion-icon>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="delete">
-                                            <ion-icon name="trash-outline" class="del"></ion-icon>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>Gien Gawesh</td>
-                                    <td>2021/IS/045</td>
-                                    <td>Software Engineer</td>
-                                    <td>02/07/2024 - 09/07/2024</td>
-                                    <td>40 mins</td>
-                                    <td>Monday</td>
-                                    <td>
-                                        <a href="scheduleInt">
-                                            <ion-icon name="create-outline"></ion-icon>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="delete">
-                                            <ion-icon name="trash-outline" class="del"></ion-icon>
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td>Shamah Lafir</td>
-                                    <td>2021/CS/085</td>
-                                    <td>Software Engineer</td>
-                                    <td>02/07/2024 - 09/07/2024</td>
-                                    <td>40 mins</td>
-                                    <td>Monday</td>
-                                    <td>
-                                        <a href="scheduleInt">
-                                            <ion-icon name="create-outline"></ion-icon>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="delete">
-                                            <ion-icon name="trash-outline" class="del"></ion-icon>
-                                        </a>
-                                    </td>
-                                </tr>
+                              
                             </tbody>
                         </table>
 
                     </div>
+
                     <div class="content content-2">
                         <div class = "cardHeader">
                             <h2>Tech-Talk Schedules</h2>
                         </div>
-                        <div id="calendar"></div>
+                        <div id="tech_talk_calendar"></div>
+
+                <!-- Bootstrap Modal for Tech-Talk Schedules -->
+                <div class="modal fade" id="techTalkModal"  role="dialog"  aria-labelledby="techTalkModalLabel">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="techTalkModalLabel">Schedule Tech Talk</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form id="techTalkForm">
+                                        <div class="form-group">
+                                            <label for="techTalkTitle">Topic</label>
+                                            <input type="text" class="form-control" id="techTalkTitle" placeholder="Enter title">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="techTalkTitle">Location</label>
+                                            <input type="text" class="form-control" id="techTalkLocation" placeholder="Enter title">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="techTalkStart" id="start">Start Date and Time</label>
+                                            <input type="datetime-local" class="form-control" id="techTalkStart" style="display:none" readonly >
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="datetime-local" class="form-control" id="techTalkEnd" style="display:none" readonly >
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Save</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
+                    </div>
+
 
                     <div class="content content-3">
                         
@@ -283,16 +188,112 @@
     </div>
 
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/5.10.1/main.min.css" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/5.10.1/main.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
 
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <script>
-   $(document).ready(function() {
-   $('#calendar').fullCalendar();
-});
+
+<script>
+    $(document).ready(function() {
+        var selectedStart;
+        var selectedEnd;
+        $('#calendar').fullCalendar();
+        $('#tech_talk_calendar').fullCalendar({
+            height: 600,
+            aspectRatio: 2,
+            contentHeight: 500,
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'agendaWeek,agendaDay'
+                },
+                dayClick: function(date, jsEvent, view) {
+                    if (date.day() === 5 && date.hour() >= 8 && date.hour() < 11) {
+                        selectedStart = date.clone().hour(8).minute(0).second(0).format('YYYY-MM-DDTHH:mm');
+                        selectedEnd = date.clone().hour(9).minute(0).second(0).format('YYYY-MM-DDTHH:mm');
+
+                        startdate = selectedStart.split('A')[0];
+                        starttime = selectedStart.split('A')[1];
+
+                        enddate = selectedEnd.split('A')[0];
+                        endtime = selectedEnd.split('A')[1];
+
+                        $('#start').text("Scheduling for the date: "+startdate+"  From:"+starttime+"  To:"+endtime);
+                        $('#techTalkModal').modal('show');
+                    } else {
+                        alert('You can only schedule tech talks on Fridays between 8 AM to 11 AM.');
+                    }
+                },
+                events: {
+                    url: '<?=ROOT?>/company/request_techtalks',
+                    type: 'GET',
+                    error: function() {
+                        alert('There was an error while fetching events!');
+                    },
+                    eventClick: function(calEvent, jsEvent, view) {
+                        alert('Event: ' + calEvent.title + '\nCompany: ' + calEvent.company + '\nLocation: ' + calEvent.location);
+                    }
+                }
+            });
+
+            $('#techTalkModal').on('shown.bs.modal', function() {
+                $('#techTalkStart').val(selectedStart);
+                $('#techTalkEnd').val(selectedEnd);
+             })
+
+        // Form submission handler
+        $('#techTalkForm').on('submit', function(e) {
+            e.preventDefault();
+
+            var title = $('#techTalkTitle').val();
+            var location = $('#techTalkLocation').val();
+
+
+            console.log("Submitting start:", selectedStart);
+            console.log("Submitting end:", selectedEnd);
+            
+            $('#techTalkModal').modal('hide');
+
+            sendScheduleData(selectedStart, selectedEnd, title, location);
+         });
+
+        function sendScheduleData(startDate, endDate, title, location) {
+
+            var formattedStartDate = startDate.replace('A', ' ').split('.')[0] + ':00';
+            var formattedEndDate = endDate.replace('A', ' ').split('.')[0] + ':00'
+
+            console.log('Formatted Start', formattedStartDate);
+            console.log('Formatted End', formattedEndDate);
+            console.log('start', startDate);
+            console.log('end', endDate);
+            console.log('title', title);
+            console.log('location', location);
+            
+            $.ajax({
+                url: '<?=ROOT?>/company/schedule_tech_talk',
+                data: {
+                    title: title,
+                    start: formattedStartDate,
+                    end: formattedEndDate,
+                    location: location
+                },
+                type: 'POST',
+                success: function(response) {
+                    console.log(response); 
+                    console.log('Tech talk scheduled successfully.');
+                    // If you want to redirect to a new page on success:
+                    //window.location.href = '<?=ROOT?>/company/schedule';
+                },
+                error: function(xhr, status, error) {
+                    console.error('Error sending schedule data: ' + error);
+                }
+            });
+        }
+    });
+
+       
+
 </script>
 </body>
 </html>
